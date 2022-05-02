6ff34c3448643c80d8c841696897e32d579ce177
abbossth <bosskodevelopment@gmail.com>
2022-05-02 18:17:25 +0500

    Data fetching

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div style="background: #DDDDDD; padding: 1rem 3rem" class="container">
+    <main>
+      <div class="search-box">
+        <input type="text" v-model="city" class="form-control" placeholder="City">
+        <button @click="getWeather()" type="button" class="btn">Find</button>
+        <div v-if="(typeof weather.main) != 'undefined'"> 
+          <p>
+            City: {{ weather.name }}, {{ weather.sys.country }}
+          </p>
+          <p style="color: ">
+            Degree: {{ Math.round(weather.main.temp) }}
+          </p>
+        </div>
+        <h1 v-if="(typeof weather) != 'undefined'" > 
+          {{ weather.sys.country }}
+        </h1>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      api_key: '240c641251721b924b1f8e5d425057a6',
+      city: 'Tashkent',
+      units: 'metric',
+      weather: {}
+    }
+  },
+  methods: {
+    async getWeather() {
+      await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.api_key}&units=${this.units}`)
+        .then(res => {
+          return res.json()
+        }).then(this.setResults)
+    },
+    setResults(results) {
+      this.weather = results
+      
+      console.log(this.weather)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
